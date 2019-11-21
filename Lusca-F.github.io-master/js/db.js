851bc1ef578d0616800716e3b001642e59f86bfb
Lusca-F <55445004+Lusca-F@users.noreply.github.com>
2019-11-21 07:49:04 -0300

    Add files via upload

--- Lusca-F.github.io-master/js/db.js
+++ Lusca-F.github.io-master/js/db.js
@@ -1,18 +1,46 @@
+// habilitar dados offline
 db.enablePersistence()
     .catch(function(err) {
         if (err.code == 'failed-precondition') {
-            console.log('persistance failed');
+            // provavelmente multiplas abas abertas ao mesmo tempo
+            console.log('Persistencia de dados falhou');
         } else if (err.code == 'unimplemented') {
-            console.log('persistance not available');
+            // browser nao suporta
+            console.log('Persistencia nao disponivel');
         }
     });
 
+// real-time listener que verifica as mudanças que ocorrem
 db.collection('Comidas').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
-            renderRecipe(change.doc.data(), change.doc.id);
+            desenhaCard(change.doc.data(), change.doc.id);
         }
         if (change.type === 'removed') {
+            // remover da pagina tambem
         }
     });
-}); 
+});
+
+// adicionar nova sobremesa
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const sobremesa = {
+        nome: form.ComidaTitulo.value,
+        descricao: form.ComidaDescricao.value,
+        link: form.ComidaLink.value,
+        endereco_imagem: form.ComidaArquivo.value
+    };
+
+    db.collection('Comidas').add(Comida)
+        .catch(err => console.log(err));
+
+    //reseta o formulario
+    form.ComidaTitulo.value = '';
+    form.ComidaDescricao.value = '';
+    form.ComidaLink.value = '';
+    form.ComidaArquivo.value = '';
+
+});
